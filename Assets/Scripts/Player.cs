b1dbb3dbdd34f2f2ca79e9aa251ee49e555f5449
Adrian Furmanek <adrian.furm@gmail.com>
2021-06-24 00:24:31 +0200

    Player Jump + Jump animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
     private void Run()
@@ -27,20 +32,38 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
         myAnimator.SetBool("Running", false);
-
-
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            myAnimator.SetBool("Running", true);
+        }
+    }
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myAnimator.SetBool("IsJumping", true);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocity;
+        }
+        if (myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myAnimator.SetBool("IsJumping", false);
+            return;
+        }
     }
     private void FlipSprite()
     {
-        if(Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis("Horizontal") > 0)
         {
             GetComponentInChildren<SpriteRenderer>().flipX = false;
-            myAnimator.SetBool("Running", true);
         }
-        else if(Input.GetAxis("Horizontal") < 0)
+        else if (Input.GetAxis("Horizontal") < 0)
         {
             GetComponentInChildren<SpriteRenderer>().flipX = true;
-            myAnimator.SetBool("Running", true);
         }
     }
 }
